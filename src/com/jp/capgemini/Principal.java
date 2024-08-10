9c2662b800fa8f756dda5ccf9a094ffb8fbb1c52
João Paulo <costajoaopaulo113@gmail.com>
2024-08-10 10:30:10 -0300

    Pratica tipo de dados

---
+++ src/com/jp/capgemini/Principal.java
@@ -0,0 +1,22 @@
+package com.jp.capgemini;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        int numero;
+        float decimalCurto;
+        double decimalLongo;
+        char caracter;
+        String nomeLongo;
+
+        numero = 24;
+        decimalCurto = 24.9f;
+        decimalLongo = 24.8;
+        caracter = 'j';
+        nomeLongo = "Jão Paulo";
+
+
+    }
+
+}
