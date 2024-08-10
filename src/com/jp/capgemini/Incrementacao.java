435d78e6511bf8a9c373234badf3d9d583f27a99
João Paulo <costajoaopaulo113@gmail.com>
2024-08-10 11:10:31 -0300

    Practice logic operators

---
+++ src/com/jp/capgemini/Incrementacao.java
@@ -0,0 +1,21 @@
+package com.jp.capgemini;
+
+public class Incrementacao {
+
+    public static void main(String[] args) {
+
+        int num = 0;
+        while (num <= 9){
+            num++;
+            System.out.println(num);
+        }
+        System.out.println("----------------------------");
+
+        int num2 = 10;
+        while (num2 > 0){
+            System.out.println(num2);
+            num2--;
+        }
+
+    }
+}
--- src/com/jp/capgemini/Principal.java
+++ src/com/jp/capgemini/Principal.java
@@ -9,12 +9,19 @@ public class Principal {
         double decimalLongo;
         char caracter;
         String nomeLongo;
+        boolean exemplo;
 
         numero = 24;
         decimalCurto = 24.9f;
         decimalLongo = 24.8;
         caracter = 'j';
         nomeLongo = "Jão Paulo";
+        exemplo = true;
+
+        System.out.println("o numero eh: " + numero);
+        System.out.println("a letra eh: " + caracter);
+        System.out.println("meu nome eh: " + nomeLongo);
+        System.out.println("o valor eh: " + exemplo);
 
 
     }
